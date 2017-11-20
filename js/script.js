639e6f06f0574b1afd00041555dac0fc33b81a10
Forever Fox <cogden@mcad.edu>
2017-11-20 04:11:45 +0200

    Initial Commit
    Still trying to figure out how to display only one at a time...

---
+++ js/script.js
@@ -0,0 +1,52 @@
+/* Borrowed from Emily Wing and Stacy Mann and http://jsfiddle.net/n53qg/:
+	$(".nav-button").click(function(e) {
+	      e.preventDefault();
+	      $(".article").fadeOut("900");
+	      $("#" + $(this).data("rel")).fadeIn("900");
+	});
+*/
+
+$(function() {
+	$("#click").click(function() {
+		$(".main-nav").toggle(reveal);
+	});
+
+	$("#recipe-1").click(function() {
+		$("#recipe-card-1").toggle();
+	});
+
+	$(".recipe-click").click(function() {
+		$(".recipe-reveal").fadeIn(800);
+	});	
+
+	$("#recipe-2").click(function() {
+		$("#recipe-card-2").toggle();
+	});
+
+	$("#recipe-3").click(function() {
+		$("#recipe-card-3").toggle();
+	});
+
+	$("#recipe-4").click(function() {
+		$("#recipe-card-4").toggle();
+	});
+
+	$("#recipe-5").click(function() {
+		$("#recipe-card-5").toggle();
+	});
+
+	$("#recipe-6").click(function() {
+		$("#recipe-card-6").toggle();
+	});	
+
+	$("#recipe-7").click(function() {
+		$("#recipe-card-7").toggle();
+	});		
+});
+
+function reveal() {
+	$(this).animate( {
+		left: "0",
+		top: "0",
+	}, 800, "swing");
+};
